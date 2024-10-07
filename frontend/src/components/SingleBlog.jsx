2e9731ca9c61b3b0a06f2db8f8b9c21e34558c86
0sourav1998 <sourav.1998.usb.sb@gmail.com>
2024-10-07 12:06:11 +0530

    Added Backend Link

--- frontend/src/components/SingleBlog.jsx
+++ frontend/src/components/SingleBlog.jsx
@@ -14,7 +14,7 @@ export const SingleBlog = () => {
   const fetchSingleBlog = async () => {
     try {
       const response = await axios.get(
-        `http://localhost:4000/api/v1/blog/singleBlog/${id}`,
+        `https://dev-diaries-xnjy.onrender.com/api/v1/blog/singleBlog/${id}`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
